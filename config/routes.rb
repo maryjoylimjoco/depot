c658c418f083ea347adb0725b6317f7fcf8140e6
Mary Joy Limjoco <maryjoylimjoco@gmail.com>
2017-08-16 14:13:14 +0800

    allow users to login or logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
 
   resources :users
   resources :orders
   resources :line_items
   resources :carts
   resources :products
+  controller :sessions do
+    get     'login'   => :new
+    post    'login'   => :create
+    delete  'logout'  => :destroy
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'store#index', as: 'store_index'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:name) {|n| "User #{n + 1}"}
+    sequence(:name) {|n| "testuser#{n + 1}"}
     password "password"
   end
 end
